18e61206349a2dd9cf24374079d6f1ca66b830f5
CarlaMelo2 <18836891-CarlaMelo2@users.noreply.replit.com>
2023-07-03 15:40:27 +0000

    Initial commit

---
+++ questions.js
@@ -0,0 +1,34 @@
+export default [
+  {
+    question: "Quais os nomes dos dragões da Daenerys Targaryen?",
+    answers: [
+      { option: "Drogon, Rhaegal e Viserion", correct: true },
+      { option: "Meraxes, Dreamfyre e Vermithor", correct: false },
+      { option: "Asaprata, Bailalua e Balerion", correct: false },
+    ],
+  },
+  {
+    question: "Qual o nome do pai da Daenerys Targaryen??",
+    answers: [
+      { option: "Maegor", correct: false },
+      { option: "Aerys", correct: true },
+      { option: "Jhaererys", correct: false },
+    ],
+  },
+  {
+    question: "Qual o nome da espada da Arya Stark??",
+    answers: [
+      { option: "Imã Sombria", correct: false },
+      { option: "Filhinha", correct: false },
+      { option: "Agulha", correct: true },
+    ],
+  },
+  {
+    question: "Qual o nome do lobo do Jon Snow??",
+    answers: [
+      { option: "Nymeria", correct: false },
+      { option: "Verão", correct: false },
+      { option: "Fantasma", correct: true },
+    ],
+  },
+];
